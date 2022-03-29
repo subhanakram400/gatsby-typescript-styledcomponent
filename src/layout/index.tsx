c5c85c5ffb854089626b63035be847b2c746957b
Muhammad Subhan Akram <97028813+subhanakram400@users.noreply.github.com>
2022-03-29 10:55:16 +0500

    boilerplate of gatsby typescript styledcomponent

---
+++ src/layout/index.tsx
@@ -0,0 +1,30 @@
+import React, { ReactChild } from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+
+import theme from '../styles/theme'
+import GlobalStyles from '../styles/globalStyles'
+
+import Head from '../components/Head'
+
+interface ILayoutProps {
+  children: ReactChild
+  location: {
+    pathname: string
+  }
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction:column
+`
+
+export default ({ children, location }: ILayoutProps) => {
+  return (
+    <Wrapper>
+      <GlobalStyles />
+      <Head pathname={location.pathname} />
+      <div>Hello this is subhan</div>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </Wrapper>
+  )
+}
